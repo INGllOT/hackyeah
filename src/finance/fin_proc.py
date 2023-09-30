4069d90d960815cff6d2c51ce684c71edd5b907e
mjste <mj.stefanik@gmail.com>
2023-10-01 01:52:21 +0200

    paragrafy

--- src/finance/fin_proc.py
+++ src/finance/fin_proc.py
@@ -1,6 +1,38 @@
-def get_fin_data(df, school_name=None, year=None, report_type=None, REGON=None):
-    df = df[df['NazwaJednostki'].str.contains(school_name)]
-    df = df[df['Rok'] == year]
-    df = df[df['TypSprawozdania'] == report_type]
-    df = df[df['REGON'] == REGON]
-    return df
+import requests
+
+_paragraphs_dochodowe = {}
+_paragraphs_wydatkowe = {}
+
+
+def get_dochodowe_paragraphs():
+    if len(_paragraphs_dochodowe) != 0:
+        return _paragraphs_dochodowe
+
+    link = "https://bestia-api.mf.gov.pl/api/paragrafy-dochodowe?limit=100&page=1"
+    while link != None:
+        result = requests.get(link)
+        if result.status_code == 200:
+            result_json = result.json()
+            # print(result_json)
+            link = result_json["links"].get("next")
+            for paragraph in result_json["data"]:
+                _paragraphs_dochodowe[paragraph["symbol"]] = paragraph["tekst"]
+
+    return _paragraphs_dochodowe
+
+
+def get_wydatkowe_paragraphs():
+    if len(_paragraphs_wydatkowe) != 0:
+        return _paragraphs_wydatkowe
+
+    link = "https://bestia-api.mf.gov.pl/api/paragrafy-wydatkowe?limit=100&page=1"
+    while link != None:
+        result = requests.get(link)
+        if result.status_code == 200:
+            result_json = result.json()
+            # print(result_json)
+            link = result_json["links"].get("next")
+            for paragraph in result_json["data"]:
+                _paragraphs_wydatkowe[paragraph["symbol"]] = paragraph["tekst"]
+
+    return _paragraphs_wydatkowe
--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout
 import input_processors.sio_reports_processor as srp
 import input_processors.financial_reports_processor as frp
 
-pupils = rs.load_sio_pupils('Kutno_HackSQL/SIO 30.09.2021.csv')
-financial_reports = frp.process_files_in_directory('Kutno_HackSQL')
+pupils = srp.load_sio_pupils('../Kutno_HackSQL/SIO 30.09.2021.csv')
+financial_reports = frp.process_files_in_directory('../Kutno_HackSQL')
 
-
+import finance.fin_proc as fp
 # if __name__ == '__main__':    
 #     app = QApplication(sys.argv)
 #     w = QWidget()    
@@ -21,5 +21,11 @@ financial_reports = frp.process_files_in_directory('Kutno_HackSQL')
 #     w.show()
 #     sys.exit(app.exec_())
 
+# print(financial_reports)
+dp = fp.get_dochodowe_paragraphs()
+wp = fp.get_wydatkowe_paragraphs()
 
+print(set(dp.keys()).intersection(set(wp.keys())))
 
+print(dp['256'])
+print(wp['256'])
