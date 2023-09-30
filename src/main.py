fb71515d521a3568623f2d7b9ec2dbaf42d7737e
mjste <mj.stefanik@gmail.com>
2023-09-30 20:50:37 +0200

    addes pyqt5

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
-import input_processors.financial_reports_processor as frp
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout
+
 import input_processors.read_SIO as rs
+import input_processors.financial_reports_processor as frp
 
-df = frp.process_files_in_directory('../Kutno_HackSQL')
-# print(df[["Dzial","Rozdzial", "Paragraf"]].drop_duplicates())
-print(df)
-
-print(rs.load_sio_pupils('../Kutno_HackSQL/SIO 30.09.2021.csv'))
+if __name__ == '__main__':    
+    app = QApplication(sys.argv)
+    w = QWidget()    
+    layout = QHBoxLayout()
+    btn = QPushButton("Hello World!")
+    layout.addWidget(btn)
+    w.setLayout(layout)
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+    sys.exit(app.exec_())
