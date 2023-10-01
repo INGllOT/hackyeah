0e3e4b24d36971530dbebc850ff141d7df2132b0
mjste <mj.stefanik@gmail.com>
2023-10-01 03:31:53 +0200

    added gui

--- src/main.py
+++ src/main.py
@@ -1,31 +1,71 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QTextEdit, QLabel
 
 import input_processors.sio_reports_processor as srp
 import input_processors.financial_reports_processor as frp
 
-pupils = srp.load_sio_pupils('../Kutno_HackSQL/SIO 30.09.2021.csv')
-financial_reports = frp.process_files_in_directory('../Kutno_HackSQL')
+pupils = srp.load_sio_pupils("../Kutno_HackSQL/SIO 30.09.2021.csv")
+financial_reports = frp.process_files_in_directory("../Kutno_HackSQL")
 
 import finance.fin_proc as fp
 
-if __name__ == '__main__':    
+class TextInput(QWidget):
+    def __init__(self, label_text, placeholder_text):
+        super().__init__()
+
+        self.label = QLabel(label_text)
+        self.label.setText(label_text)
+
+        self.text_edit = QTextEdit()  
+        self.text_edit.setPlaceholderText(placeholder_text)
+        self.text_edit.setFixedHeight(30)
+
+        layout = QHBoxLayout()
+        self.setLayout(layout)
+
+        layout.addWidget(self.label)
+        layout.addWidget(self.text_edit)
+
+
+def left_menu_def():
+    widget = QWidget()
+    layout = QVBoxLayout()
+    widget.setLayout(layout)
+
+    widget1 = TextInput("Nazwa szkoły", "Wpisz nazwę szkoły")
+    layout.addWidget(widget1)
+
+    widget2 = TextInput("REGON", "Wpisz REGON")
+    layout.addWidget(widget2)
+
+    return widget
+
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    w = QWidget()    
+    w = QWidget()
+    w.resize(1200, 800)
+    w.move(300, 100)
+    w.setWindowTitle("")
+
     layout = QHBoxLayout()
-
-    left_layout = QVBoxLayout()
-    right_layout = QVBoxLayout()
-
-
-    btn = QPushButton("Hello World!")
-    layout.addWidget(btn)
     w.setLayout(layout)
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
+
+    left_menu = left_menu_def()
+    layout.addWidget(left_menu)
+    left_menu.setFixedSize(300, 800)
+
+    right_page = QWidget()
+    layout.addWidget(right_page)
+    right_page.setFixedSize(900, 800)
+
+    # btn = QPushButton("Hello World!")
+    # layout.addWidget(btn)
+
     w.show()
     sys.exit(app.exec_())
+
 
 # print(financial_reports)
 # dp = fp.get_dochodowe_paragraphs()
