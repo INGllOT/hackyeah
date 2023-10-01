5d25ed762e2a06fe4429e32e695e644ddc6dd62c
mjste <mj.stefanik@gmail.com>
2023-10-01 02:08:55 +0200

    layouts

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout
 
 import input_processors.sio_reports_processor as srp
 import input_processors.financial_reports_processor as frp
@@ -8,24 +8,30 @@ pupils = srp.load_sio_pupils('../Kutno_HackSQL/SIO 30.09.2021.csv')
 financial_reports = frp.process_files_in_directory('../Kutno_HackSQL')
 
 import finance.fin_proc as fp
-# if __name__ == '__main__':    
-#     app = QApplication(sys.argv)
-#     w = QWidget()    
-#     layout = QHBoxLayout()
-#     btn = QPushButton("Hello World!")
-#     layout.addWidget(btn)
-#     w.setLayout(layout)
-#     w.resize(250, 150)
-#     w.move(300, 300)
-#     w.setWindowTitle('Simple')
-#     w.show()
-#     sys.exit(app.exec_())
+
+if __name__ == '__main__':    
+    app = QApplication(sys.argv)
+    w = QWidget()    
+    layout = QHBoxLayout()
+
+    left_layout = QVBoxLayout()
+    right_layout = QVBoxLayout()
+
+
+    btn = QPushButton("Hello World!")
+    layout.addWidget(btn)
+    w.setLayout(layout)
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+    sys.exit(app.exec_())
 
 # print(financial_reports)
-dp = fp.get_dochodowe_paragraphs()
-wp = fp.get_wydatkowe_paragraphs()
+# dp = fp.get_dochodowe_paragraphs()
+# wp = fp.get_wydatkowe_paragraphs()
 
-print(set(dp.keys()).intersection(set(wp.keys())))
+# print(set(dp.keys()).intersection(set(wp.keys())))
 
-print(dp['256'])
-print(wp['256'])
+# print(dp['256'])
+# print(wp['256'])
